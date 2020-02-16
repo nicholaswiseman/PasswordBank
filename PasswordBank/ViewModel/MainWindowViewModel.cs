63b97ea12fce0dd97a75181e554e6c6b4d3c1c9f
nicholaswiseman <nicholas.jw.wiseman@gmail.com>
2020-02-16 11:42:25 -0400

    Adding initial project.

---
+++ PasswordBank/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PasswordBank.ViewModel
+{
+    class MainWindowViewModel
+    {
+
+        public string Password { get; set; }
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            this.Password = "hello";
+        }
+
+    }
+}
