6f6e8e0ca03a6a4352f2afb224440903c85a93b3
nicholaswiseman <nicholas.jw.wiseman@gmail.com>
2020-02-17 12:10:32 -0400

    Implement Add button functionality.

---
+++ PasswordBank/Model/MainWindowModel.cs
@@ -0,0 +1,111 @@
+﻿using PasswordBank.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace PasswordBank.Model
+{
+    class MainWindowModel
+    {
+        public XDocument XmlDatabase
+        {
+            get
+            {
+                return _xmlDatabase;
+            }
+            set
+            {
+                _xmlDatabase = value;
+                this.ViewModel.PasswordList = this.GetPasswordList();
+            }
+        }
+
+        public string XmlPath { get; set; }
+
+        public MainWindowViewModel ViewModel { get; set; }
+
+        private XDocument _xmlDatabase;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public MainWindowModel(MainWindowViewModel vm)
+        {
+            this.ViewModel = vm;
+
+            InitXmlDatabase();
+        }
+
+        public void InitXmlDatabase()
+        {
+                this.XmlPath = $@"{Directory.GetCurrentDirectory()}/database.xml";
+
+                if (!File.Exists(XmlPath))
+                {
+                    CreateXmlDatabase();
+                }
+                else
+                {
+                    XmlDatabase = XDocument.Load(XmlPath);
+                }
+
+        }
+
+        public void AddToXml(string service, string password)
+        {
+            XmlDatabase.Element("logins").Add(new XElement("login",
+                new XElement("service", service),
+                new XElement("pw", password)));
+            XmlDatabase.Save(XmlPath);
+            //XmlDatabase = XDocument.Load(XmlPath);
+
+        }
+
+        public ObservableCollection<Login> GetPasswordList()
+        {
+            ObservableCollection<Login> res = new ObservableCollection<Login>();
+            try
+            {
+                foreach (var element in XmlDatabase.Element("logins").Descendants("login"))
+                {
+
+                    string service = element.Element("service").Value;
+                    string pw = element.Element("pw").Value;
+                    res.Add(new Login(service, pw));
+
+                }
+            }
+            catch
+            {
+
+            }
+
+            return res;
+        }
+
+        public void CreateXmlDatabase()
+        {
+            this.XmlDatabase = new XDocument();
+            XmlDatabase.Add(new XElement("logins"));
+            XmlDatabase.Save(XmlPath);
+        }
+    }
+
+    public class Login
+    {
+        public string Service { get; set; }
+        public string Password { get; set; }
+
+        public Login(string service, string pw)
+        {
+            this.Service = service;
+            this.Password = pw;
+        }
+    }
+}
--- PasswordBank/ViewModel/MainWindowViewModel.cs
+++ PasswordBank/ViewModel/MainWindowViewModel.cs
@@ -1,11 +1,14 @@
-﻿using PasswordBank.Model;
+﻿using GalaSoft.MvvmLight.Command;
+using PasswordBank.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Input;
 using System.Xml.Linq;
 
 namespace PasswordBank.ViewModel
@@ -13,7 +16,11 @@ namespace PasswordBank.ViewModel
     class MainWindowViewModel
     {
 
-        public List<Login> PasswordList { get; set; }
+        public ObservableCollection<Login> PasswordList { get; set; }
+        public string PasswordTextBox { get; set; }
+        public string ServiceTextBox { get; set; }
+
+        public ICommand AddButtonCommand { get; set; }
 
         private MainWindowModel _model;
 
@@ -24,8 +31,16 @@ namespace PasswordBank.ViewModel
         {
             //link model and viewmodel
             this._model = new MainWindowModel(this);
+            this.AddButtonCommand = new RelayCommand(AddButton);
+            this.PasswordTextBox = "";
+            this.ServiceTextBox = "";
         }
 
+        public void AddButton()
+        {
+            this.PasswordList.Add(new Login(ServiceTextBox, PasswordTextBox));
+            this._model.AddToXml(ServiceTextBox, PasswordTextBox);
+        }
 
     }
 }
